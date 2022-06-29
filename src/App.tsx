de821aee93cacc61467d233e33b1fc56b988262f
Gregfanyan <grigorfanyan@gmail.com>
2022-06-29 12:40:14 +0200

    ✨ fetching the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import useApiRequest from "./hooks/useApiRequest";
 
 function App() {
+  const url = "https://api-stg.jam-community.com/song/trending";
+  const { songs, error, isLoaded } = useApiRequest(url);
+  console.log("songs", songs);
   return <div className="App"></div>;
 }
 
