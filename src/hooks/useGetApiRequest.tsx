fedfa92ad88ce7efc5ca57c317abc7a22f76d480
Gregfanyan <grigorfanyan@gmail.com>
2022-06-30 19:20:48 +0200

    🔥 remove unused files

--- src/hooks/useGetApiRequest.tsx
+++ src/hooks/useGetApiRequest.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+
 const useGetApiRequest = (url: string) => {
   const [songs, setSongs] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
